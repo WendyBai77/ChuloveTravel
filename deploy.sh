025e331ecd6a425a2a61a4a0ee0920a4298a4534
wendy <candy1992517@gmail.com>
2023-02-21 15:49:34 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/wendybai77/ChuloveTravel.git main
+# git push -f git@github.com:WendyBai77/ChuloveTravel.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f https://github.com/wendybai77/ChuloveTravel.git main:gh-pages
+git push -f https://github.com/wendybai77/ChuloveTravel.git main:gh-pages
 
 cd -
